4d800005e058c34fbee27dc2e588386754696101
Sanskar-Rijal <rijalsanskar@gmail.com>
2024-09-20 18:13:54 +0545

    made each row clickable

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.movies
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -13,10 +32,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.movies.ui.theme.MoviesTheme
+import org.intellij.lang.annotations.PrintFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +67,7 @@ fun myapp( content: @Composable ()-> Unit) {
         Scaffold(
             topBar = {
                 TopAppBar(title =
-                { Text(text = "hello welcome") }
+                { Text(text = "hello") }
                 , colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary
                 , titleContentColor = Color.White)
                     )
@@ -60,11 +83,62 @@ fun myapp( content: @Composable ()-> Unit) {
 
 
 @Composable
-fun MainContent()
+fun MainContent(moveList:List<String> = listOf(
+    "3 Idiots",
+    "Chhichhore",
+    "Stree",
+    "Dhamaal",
+    "Spiderman Noway home",
+    "Munna Bhai M.B.B.S",
+    "Ghajini",
+    "PK",
+    "Hera Pheri",
+    "Drishyam",
+    "Avatar"
+))
 {
-    Surface(color = MaterialTheme.colorScheme.background
+    Column( modifier = Modifier.padding(12.dp)) {
+    //creating a recycler view
+    LazyColumn{
+        items(items =  moveList)
+        {
+            MovieRow(it){movie->
+                Log.d("TAG", "MainContent: $movie")
+            }
+        }
+    }
+    }
+}
 
+@Composable
+fun MovieRow(movie:String="sanskar",onItemClick:(String)->Unit={})
+//onItemClick is of type string and it returns nothing , if we give = {} then
+//we are saying at default we are not passing any thing
+{
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(4.dp)
+        .height(130.dp)
+        .clickable {
+                   onItemClick(movie)
+        }
+        , elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+        ,shape = RoundedCornerShape(corner = CornerSize(15.dp)
+            )
     ) {
-        Text(text = "hello")
+        Row(verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start) {
+            Surface(modifier = Modifier
+                .padding(12.dp)
+                .size(100.dp)
+                , shape = RectangleShape
+                , shadowElevation = 10.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox,
+                    contentDescription ="Image",
+                    )
+            }
+            Text(text = movie)
+        }
     }
 }
